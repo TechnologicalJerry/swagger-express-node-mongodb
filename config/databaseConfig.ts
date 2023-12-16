8f6ac40d98dddea46ff6530cd941e8c8fd5cecc9
technologicaljerry <cserajatverma@gmail.com>
2023-12-16 23:35:03 +0530

    Update databaseConfig.ts

--- config/databaseConfig.ts
+++ config/databaseConfig.ts
@@ -3,13 +3,7 @@ import config from "config";
 import log from "./logger";
 
 async function databaseConnection() {
-  const dbUrl = config.get<string>('dbUrl');
-  try {
-    await mongoose.connect(dbUrl);
-    log.info("Connected to DB");
-  } catch (error) {
-    process.exit(1);
-  }
+  
 }
 
 export default databaseConnection;
